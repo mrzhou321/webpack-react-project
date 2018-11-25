d546a578c1140a99f7254353051b976616a12d2e
PaulChou <paulchou@PaulChoudeMacBook-Pro.local>
2018-11-25 22:28:10 +0800

    modify gitignore again

---
+++ build/webpack.base.config.js
@@ -0,0 +1,40 @@
+/**
+ * @Auther:  Paul Chou
+ * @CreatedAt: 2018-11-22 22:24
+ * @Project: webpack-blog
+ * Since you created this file, you should make it perfect .
+ */
+
+ const path = require('path')
+ const HTMLPlugin = require('html-webpack-plugin')
+
+ module.exports = {
+     // 应用入口
+     entry:{
+       app: path.join(__dirname, '../src/main.js')
+     },
+     // 输出目录
+     output: {
+         filename: "[name].[hash].js", // name代表entry对应的名字；hash代表整个app打包完成后根据内容加上hash
+         path: path.join(__dirname, '../dist'),
+         publicPath: "/publick"
+     },
+     module: {
+         rules:[
+             {
+                 test: /.jsx$/,
+                 loader: 'babel-loader'
+             },
+             {
+                 test: /.(js)$/,
+                 loader: 'babel-loader',
+                 exclude: [
+                     path.join(__dirname, '../node_modules')
+                 ]
+             }
+         ]
+     },
+     plugins: [
+         new HTMLPlugin()
+     ]
+ }
